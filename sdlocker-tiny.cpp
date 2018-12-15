42cb1f861822e66ce5fe1a7f5f9ae7c84d01e383
CRImier <crimier@yandex.ru>
2018-12-15 03:36:04 +0200

    I have broken pinout on rev1 of my PCBs, working around some of it in software

--- sdlocker-tiny.cpp
+++ sdlocker-tiny.cpp
@@ -118,8 +118,13 @@
 /*
  * Define bits used by the SPI port.
  */
-#define MOSI_BIT    0
-#define MISO_BIT    1
+#if BROKEN_PINOUT
+  #define MOSI_BIT    1
+  #define MISO_BIT    0
+#else
+  #define MOSI_BIT    0
+  #define MISO_BIT    1
+#endif
 #define SCK_BIT     2
 #define CS_BIT      3
 
